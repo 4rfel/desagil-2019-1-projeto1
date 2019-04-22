3b1ab9336825d183db741049d639956d23c4f188
Lucas Vale <lucaslealvale01@gmail.com>
2019-04-22 11:16:14 -0300

    listas de alfabeto e morse

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
@@ -16,6 +16,18 @@ public class Translator {
     public Translator() {
         root = null;
         map = null;
+        // listas de alfabeto e morse presentes no site https://www.ssaurel.com/blog/learn-to-create-a-morse-code-converter-app-for-android/ , acesso 22/04 todo credito a s.saurel
+        String[] Alfabeto = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", ",", "?",
+                ".", "'" };
+
+        String[] Morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+                "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "-.-.--", "--..--",
+                "..--..", ".-.-.-", ".----.", };
+
+        HashMap<String, String> charToMorse = new HashMap<>();
+        HashMap<String, String> morseToChar = new HashMap<>();
     }
 
 
