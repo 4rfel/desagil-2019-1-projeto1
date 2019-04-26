43126d77536e6ec9175d16f2e544093df61b94d3
slimkaki <rafaalmada98@gmail.com>
2019-04-26 19:42:09 -0300

    aprimorando algumas funcionalidades e adicionando botao de enviar

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
@@ -25,66 +26,57 @@ public class SMSActivity extends AppCompatActivity {
         toast.show();
     }
 
+    private String modifyText;
+    private long lastClickTime=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
 
-
-        EditText textMessage = findViewById(R.id.text_message);
-//        textMessage.setOnEditorActionListener(textMessageListener);
-        EditText textPhone = findViewById(R.id.text_phone);
-        //Button buttonSend = findViewById(R.id.button_send);
+        TextView textMessage = findViewById(R.id.text_message);
+        TextView textPhone = findViewById(R.id.text_phone);
+        Button buttonSend = findViewById(R.id.button_send);
         Button buttonDigit = findViewById(R.id.button_digit);
+        Button buttonDelete = findViewById(R.id.button_delete);
 
-//    private TextView.OnEditorActionListener textMessageListener = new TextView.OnEditorActionListener() {
-//        @Override
-//        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//            switch (actionId) {
-//                case EditorInfo.IME_
-//            }
-//            return false;
-//        }
-//    };
+        final long bootTime = SystemClock.elapsedRealtime();
+        lastClickTime = bootTime;
+
+        textMessage.setText("");
+
+        // Botão do ponto ou barra
         buttonDigit.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                //LinkedList<String> digitado = new LinkedList<>()
-                String texto = "";
-                texto = texto.concat(".");
-                //digitado.add(".");
-                textMessage.setText(texto);
+                Long timeNow = SystemClock.elapsedRealtime();
+                Long timeElapsed = SystemClock.elapsedRealtime() - lastClickTime;
+                if (timeElapsed > 950.0) {
+                    textMessage.append("-");
+                } else {
+                    textMessage.append(".");
+                }
+                lastClickTime = timeNow;
                 }
             });
-//        buttonSend.setOnClickListener((view) -> {
-//            String message = textMessage.getText().toString();
-//
-//            if (message.isEmpty()) {
-//                showToast("Mensagem inválida!");
-//                return;
-//            }
-//
-//            String phone = textPhone.getText().toString();
-//
-//            // Esta verificação do número de telefone é bem
-//            // rígida, pois exige até mesmo o código do país.
-//            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-//                showToast("Número inválido!");
-//                return;
-//            }
-//
-//            // Enviar uma mensagem de SMS. Por simplicidade,
-//            // não estou verificando se foi mesmo enviada,
-//            // mas é possível fazer uma versão que verifica.
-//            SmsManager manager = SmsManager.getDefault();
-//            manager.sendTextMessage(phone, null, message, null, null);
-//
-//            // Limpar o campo para nenhum engraçadinho
-//            // ficar apertando o botão várias vezes.
-//            textPhone.setText("");
-//        });
+
+        // Botão Delete
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Long timeNow = SystemClock.elapsedRealtime();
+                Long timeElapsed = SystemClock.elapsedRealtime() - lastClickTime;
+                if (timeElapsed > 950.0) {
+                    textMessage.setText("");
+                } else {
+                    modifyText = textMessage.getText().toString();
+                    modifyText = modifyText.substring(0, modifyText.length() - 1);
+                }
+                lastClickTime = timeNow;
+            }
+        });
     }
 }
 
