f1178f7f9f6e6ca9810a354afb7494e9d32ee3b5
4rfel <38050598+4rfel@users.noreply.github.com>
2019-04-28 12:49:09 -0300

    n sei se ta 100%

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -3,6 +3,8 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -71,5 +73,36 @@ public class SMSActivity extends AppCompatActivity {
                 lastClickTime = timeNow;
             }
         });
+
+        // Botão Envio
+        buttonSend.setOnClickListener((view) -> {
+            String message = textMessage.getText().toString();
+
+            if (message.isEmpty()) {
+                showToast("Mensagem inválida!");
+                return;
+            }
+
+            String phone = textPhone.getText().toString();
+
+            // Esta verificação do número de telefone é bem
+            // rígida, pois exige até mesmo o código do país.
+            if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+                showToast("Número inválido!");
+                return;
+            }
+
+            // Enviar uma mensagem de SMS. Por simplicidade,
+            // não estou verificando se foi mesmo enviada,
+            // mas é possível fazer uma versão que verifica.
+            SmsManager manager = SmsManager.getDefault();
+            manager.sendTextMessage(phone, null, message, null, null);
+
+            // Limpar o campo para nenhum engraçadinho
+            // ficar apertando o botão várias vezes.
+            textPhone.setText("");
+        });
+
+
     }
 }
