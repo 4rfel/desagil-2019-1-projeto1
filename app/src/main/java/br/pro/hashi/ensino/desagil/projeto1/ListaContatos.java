922aea8839a87934f0248f7dd9fa53a81cab3d79
Pedro Fontes <pvfontes1000@gmailcom>
2019-05-08 08:54:32 -0300

    classes prontas, falta implementação no app

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ListaContatos.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ListaContatos.java
@@ -1,19 +1,26 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
-import
-
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class ListaContatos {
-    private Map lista;
+    private Map<String, String> lista;
 
     public ListaContatos(){
-        Map lista = new HashMap<>();
+        this.lista = new HashMap<>();
     }
 
     public void addContato(Contato pessoa){
         lista.put(pessoa.getName(), pessoa.getNum());
+
     }
 
+    public String getContact(){
+        for (Map.Entry<String, String> entrada : lista.entrySet()){
+            System.out.println("Nome = " + entrada.getKey() + "Número = " + entrada.getValue());
+        }
+
+    }
 }
+
