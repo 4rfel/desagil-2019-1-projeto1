7dd91fd1818f8034ce8e7247fa85ad9346fc23f6
slimkaki <rafaalmada98@gmail.com>
2019-05-08 13:27:15 -0300

    Mensagem pronta selecionando por cor

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MsgPronta.java
@@ -0,0 +1,47 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+
+public class MsgPronta {
+
+    private TextView message;
+    private boolean flag; // A flag diz se a menssagem está selecionada ou não
+    private Context context;
+
+    public MsgPronta (TextView message, Context context) {
+        this.message = message;
+        this.flag = false;
+        this.context = context;
+    }
+
+    public TextView getMessage() {
+        return message;
+    }
+
+    public void setFlagTrue() {
+        flag = true;
+    }
+
+    public void setFlagFalse() {
+        flag = false;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public String getMessageString() {
+        return message.getText().toString();
+    }
+
+    public void paintMessage() {
+        if (this.flag) {
+            message.setTextColor(ContextCompat.getColor(context, R.color.blue));
+        } else {
+            message.setTextColor(ContextCompat.getColor(context, R.color.black));
+        }
+    }
+}
