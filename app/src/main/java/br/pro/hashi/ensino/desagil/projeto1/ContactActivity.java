0144c331611de90bdae04bd53d9779d3e2802573
slimkaki <rafaalmada98@gmail.com>
2019-05-10 19:59:37 -0300

    estruturando activity

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactActivity.java
@@ -1,12 +1,20 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class ContactActivity extends AppCompatActivity {
 
-    private Button backButton;
+    protected Button backButton;
+    protected Button upButton;
+    protected Button downButton;
+    protected Button selectButton;
+
+    private int contactCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +22,44 @@ public class ContactActivity extends AppCompatActivity {
 
         backButton = findViewById(R.id.backButton);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ContactActivity.this, SMSActivity.class);
+                setResult(2, intent);
+                finish();
+            }
+        });
+
+        upButton = findViewById(R.id.upButton);
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        downButton = findViewById(R.id.downButton);
+
+        downButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        selectButton = findViewById(R.id.selectButton);
+
+        selectButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
